a1034071606a68c65f5fcfbbc522f1a6bc4a93fc
olegnovatskiy <olegnovatskiy@gmail.com>
2017-03-27 10:36:51 +0300

    init project

---
+++ src/main/java/com/Bot/HakatonApplication.java
@@ -0,0 +1,12 @@
+package com.Bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HakatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HakatonApplication.class, args);
+	}
+}
